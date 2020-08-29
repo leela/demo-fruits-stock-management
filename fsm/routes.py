7689602bbe84f9faff0a08510a90efbe8290e91a
leela <leela.vadlamudi@gmail.com>
2020-08-29 18:05:17 +0530

    demo-3
    Introduce templates in the application

--- fsm/routes.py
+++ fsm/routes.py
@@ -1,5 +1,12 @@
+from flask import render_template
 from fsm import app
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def index():
+    fruits = [
+        {'name': 'Banana', 'price': 20.0, 'quantity': 100},
+        {'name': 'Apple', 'price': 100.0, 'quantity': 50},
+        {'name': 'Guava', 'price': 40.0, 'quantity': 120},
+        {'name': 'Pomegranate', 'price': 60.0, 'quantity': 200},
+    ]
+    return render_template("index.html", fruits=fruits)
