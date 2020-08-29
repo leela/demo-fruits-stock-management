33283bc6006dcf6362c9cde2ea55fa600d3e7b26
leela <leela.vadlamudi@gmail.com>
2020-08-29 18:46:04 +0530

    demo-5
    Introduce forms by adding view to add fruits in the system.

--- fsm/config.py
+++ fsm/config.py
@@ -2,3 +2,4 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, 'app.db')
+SECRET_KEY = "my_secret_key"
--- fsm/routes.py
+++ fsm/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import (render_template, url_for, request, flash,
+    redirect)
 from fsm import app, config
 import web
 
@@ -8,3 +9,12 @@ db = web.database(config.DATABASE_URL)
 def index():
     fruits = db.select("fruits").list()
     return render_template("index.html", fruits=fruits)
+
+@app.route("/fruits", methods=['GET', 'POST'])
+def fruits():
+    if request.method == 'POST':
+        name, price = request.form['fruit'], request.form['price']
+        db.insert("fruits", name=name, price= float(price))
+        flash(f"Fruit {name} Added to the system")
+        return redirect(url_for("index"))
+    return render_template("add_fruit.html")
